d53fb38dd177a202b67c142bbdc628c210843cba
Huynh Tan Khoa <cuonglieu3256@gmail.com>
2022-03-01 23:56:11 +0700

    GAE-8 | feat(authentication): add view and angular dashboard

--- erp_greenwich/static/js/project.js
+++ erp_greenwich/static/js/project.js
@@ -1,1 +1,26 @@
-/* Project specific Javascript goes here. */
+(function () {
+    if (document.getElementById("get-current-year")) {
+        document.getElementById(
+            "get-current-year"
+        ).innerHTML = new Date().getFullYear();
+    }
+})();
+
+/* Function for opning navbar on mobile */
+function toggleNavbar(collapseID) {
+    document.getElementById(collapseID).classList.toggle("hidden");
+    document.getElementById(collapseID).classList.toggle("block");
+}
+
+/* Function for dropdowns */
+function openDropdown(event, dropdownID) {
+    let element = event.target;
+    while (element.nodeName !== "A") {
+        element = element.parentNode;
+    }
+    Popper.createPopper(element, document.getElementById(dropdownID), {
+        placement: "bottom-start",
+    });
+    document.getElementById(dropdownID).classList.toggle("hidden");
+    document.getElementById(dropdownID).classList.toggle("block");
+}
