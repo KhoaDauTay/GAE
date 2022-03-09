cdafc78a1b1bd567a5bd00f635e56e12094a19db
Huynh Tan Khoa <cuonglieu3256@gmail.com>
2022-03-09 11:47:01 +0700

    GAE-39 | build(kong): add cors allow all

--- config/settings/base.py
+++ config/settings/base.py
@@ -318,9 +318,11 @@ REST_FRAMEWORK = {
 
 # django-cors-headers - https://github.com/adamchainz/django-cors-headers#setup
 CORS_URLS_REGEX = r"^/api/.*$"
-CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS  =  [
+    "https://gae-gw.systems ,
+    "http://localhost:3000" ,
+    "http://localhost:4200" ,
+]
 # Casbin
 # ------------------------------------------------------------------------------
 CASBIN_MODEL = os.path.join(APPS_DIR, "auth", "casbin", "role_rbac.conf")
